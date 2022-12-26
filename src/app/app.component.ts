00fc1a2ea88b769dfb8453fbd4ad5ae9fb16ba08
Matheus Cardoso <92444207+user-matth@users.noreply.github.com>
2022-12-26 16:23:13 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pomodoro-timer';
+
+  minutes: number = 25
+  seconds: number = 0
+  intervalId: any
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  increaseMinutes(){
+    this.minutes = this.minutes + 1
+  }
+
+  decreaseMinutes(){
+    this.minutes = this.minutes - 1
+  }
+
+  startTimer() {
+    this.intervalId = setInterval(() => {
+      if (this.seconds > 0) {
+        this.seconds--;
+      } else if (this.minutes > 0) {
+        this.minutes--;
+        this.seconds = 59;
+      }
+    }, 1000);
+  }
+
+  stopTimer() {
+    clearInterval(this.intervalId);
+  }
+
+  restartTimer(){
+    this.minutes = 25
+    this.seconds = 0
+  } 
+  
 }
